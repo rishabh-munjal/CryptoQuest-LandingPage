a88ffdd0507ec586520f3ce6aee664c293b0dc2b
Rishabh Munjal <rishabh5566ind1111@gmail.com>
2024-12-22 00:42:43 +0530

    jdsf

---
+++ src/script.js
@@ -0,0 +1,19 @@
+gsap.from('#herotext',{
+    duration: 0.7,
+    x: -1000,
+});
+
+gsap.from('#tagline',{
+    opacity: 0,
+    delay: 0.7,
+    duration: 0.7,
+});
+
+gsap.from('#hero-image-1',{
+    y: -1000,
+    duration: 0.7,
+});
+gsap.from('#hero-image-2',{
+    x: 1000,
+    duration: 0.7,
+});
